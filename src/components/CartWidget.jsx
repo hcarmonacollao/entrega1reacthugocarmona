e722d77ce9607fcb15fb9dba86d26f69e0e10242
hugocarmona <hcarmonacollao@gmail.com>
2023-12-04 11:00:04 -0300

    Cambios en CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,8 @@
 
 function CartWidget() {
-  const contador =15;
   return (
     <div style={{display: 'flex', width: 30, justifyContent: 'space-between', alignItems: 'center'}}>
-        🛒<span style={{fontWeight: 'bold', fontSize: '1.1rem'}}>{contador}</span>
+        🛒<span style={{fontWeight: 'bold', fontSize: '1.1rem'}}>10</span>
     </div>
   )
 }
