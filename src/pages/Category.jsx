643c08b3aa071ab5398eba84d6bb6b96cad34610
hugocarmona <hcarmonacollao@gmail.com>
2023-12-22 20:02:48 -0300

    Avance Segunda Entrega

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom'
-import ItemListContainer from '../components'
+import ItemListContainer from '../components/ItemListContainer'
 import { UseGetProductByCategory } from '../hooks/UseGetProductsByCategory';
 
 export const Category = () => {
     const {id} = useParams();
-    const {productsData} = UseGetProductByCategory(id)
+    const {productData} = UseGetProductByCategory(id)
   return (
-    <ItemListContainer greeting="Bienvenidos a La Tiendita" productsData={productsData}/>
+    <ItemListContainer greeting="Bienvenidos a La Tiendita" productsData={productData}/>
   )
 }
